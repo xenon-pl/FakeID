459e7ff23eaa0d26f2efea822f155e7f15993dd9
niko! <200838252+xenon-pl@users.noreply.github.com>
2026-02-27 17:54:27 +0000

    Add files via upload

---
+++ Mod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+
+namespace FakeID
+{
+    [BepInPlugin("xenon.fake.id", "Fake ID", "1.0.0")]
+    public class Mod : BaseUnityPlugin
+    {
+        void Awake() => new Harmony("xenon.fake.id").PatchAll();
+    }
+    
+    [HarmonyPatch(typeof(KIDManager))]
+    [HarmonyPatch("KidEnabled", MethodType.Getter)]
+    internal class IDPatch
+    {
+        private static bool Prefix(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
+}
